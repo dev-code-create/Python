ecf2ff48ac8ac80e7b9689defbb3ee53f597b3cd
dev-code-create <ayushkankale04@gmail.com>
2026-01-08 23:57:04 +0530

    kmeans

---
+++ unsupervised/Algorithm/Kmeans_clustering.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+# sample data
+X = np.array([
+    [1, 2], [1, 4], [1, 0],
+    [10, 2], [10, 4], [10, 0]
+])
+
+kmeans = KMeans(n_clusters=2, random_state=42)
+kmeans.fit(X)
+
+labels = kmeans.labels_
+centers = kmeans.cluster_centers_
+
+print(labels)
+print(centers)
