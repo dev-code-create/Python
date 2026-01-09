4833a6f4859f2d7c6251d762b94f4c92a502607e
dev-code-create <ayushkankale04@gmail.com>
2026-01-09 23:23:56 +0530

    Update Kmeans_clustering.py

--- unsupervised/Algorithm/Kmeans_clustering.py
+++ unsupervised/Algorithm/Kmeans_clustering.py
@@ -4,8 +4,13 @@ from sklearn.cluster import KMeans
 
 # sample data
 X = np.array([
-    [1, 2], [1, 4], [1, 0],
-    [10, 2], [10, 4], [10, 0]
+   #Income and Spending
+    [1, 2],
+    [1, 4],
+    [1, 0],
+    [10, 2], 
+    [10, 4], 
+    [10, 0]
 ])
 
 kmeans = KMeans(n_clusters=2, random_state=42)
