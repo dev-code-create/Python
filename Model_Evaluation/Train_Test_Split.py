f56178470b1a5cae38aa7e1539f837fc3438cb66
dev-code-create <ayushkankale04@gmail.com>
2026-01-12 11:31:51 +0530

    Update Train_Test_Split.py

--- Model_Evaluation/Train_Test_Split.py
+++ Model_Evaluation/Train_Test_Split.py
@@ -1,4 +1,8 @@
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+
+model = LogisticRegression()
 
 X = [[1], [2], [3], [4], [5], [6]]
 y = [0, 0, 0, 1, 1, 1]
@@ -6,3 +10,7 @@ y = [0, 0, 0, 1, 1, 1]
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42
 )
+
+scores = cross_val_score(model,X,y,cv=3)
+print(scores)
+print(scores.mean())
