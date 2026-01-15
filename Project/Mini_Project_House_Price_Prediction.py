a0d975b609280bf30eec0b7e44878a40ce89ee33
dev-code-create <ayushkankale04@gmail.com>
2026-01-15 23:46:49 +0530

    Info data

--- Project/Mini_Project_House_Price_Prediction.py
+++ Project/Mini_Project_House_Price_Prediction.py
@@ -9,3 +9,16 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
+
+data = {
+    "Area": [800, 900, 1000, 1100, 1200, 1500, 1800],
+    "Bedrooms": [1, 2, 2, 3, 3, 4, 4],
+    "Bathrooms": [1, 1, 2, 2, 2, 3, 3],
+    "Location": ["City", "City", "Suburb", "Suburb", "City", "City", "Suburb"],
+    "Price": [40, 50, 55, 65, 70, 90, 100]  # in lakhs
+}
+
+df = pd.DataFrame(data)
+
+df.info()
+df.describe()
